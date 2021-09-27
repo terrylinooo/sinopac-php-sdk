cf46adbbeb01ca6581f02558990f84b568976f45
Terry Lin <terrylin.developer@gmail.com>
2021-09-28 00:35:51 +0800

    Impove examples.

---
+++ tests/Examples/03_query_orders.php
@@ -0,0 +1,26 @@
+<?php
+
+defined('PHPUNIT_TEST') || exit('Example code is not allowed to access.');
+
+/**
+ * Example 03. Query a deluge of orders.
+ */
+include __DIR__ . '/../../autoload.php';
+
+$qpay = new \Sinopac\QPay();
+$qpay->setShopNo('NA0249_001');
+$qpay->setFirstHashPair('86D50DEF3EB7400E', '01FD27C09E5549E5');
+$qpay->setSecondHashPair('9E004965F4244953', '7FB3385F414E4F91');
+$qpay->enableSandbox();
+
+$data = [
+    'pay_type'             => 'A',
+    'order_datetime_begin' => '202109250110',
+    'order_datetime_end'   => '202109262359'
+];
+
+$results = $qpay->queryOrders($data);
+
+if (!empty($results['Message'])) {
+    print_r($results['Message']);
+}
---
+++ tests/Examples/04_query_order_by_token.php
@@ -0,0 +1,21 @@
+<?php
+
+defined('PHPUNIT_TEST') || exit('Example code is not allowed to access.');
+
+/**
+ * Example 04. Query a single order's information by using its PayToken.
+ */
+include __DIR__ . '/../../autoload.php';
+
+$qpay = new \Sinopac\QPay();
+$qpay->setShopNo('NA0249_001');
+$qpay->setFirstHashPair('86D50DEF3EB7400E', '01FD27C09E5549E5');
+$qpay->setSecondHashPair('9E004965F4244953', '7FB3385F414E4F91');
+$qpay->enableSandbox();
+
+$token = 'da1547c3d0d1649af5049125b0880c0e227f31e107cbf4f0995bed28d0f066c1';
+$results = $qpay->queryOrderByToken($token);
+
+if (!empty($results['Message'])) {
+    print_r($results['Message']);
+}
